e2141d92f1637d9a897456b8bbd9e30a92a68d7c
afushimi-source <tanafushi@gmail.com>
2021-08-16 11:32:36 +0900

    add null: false to title column

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_075419) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
