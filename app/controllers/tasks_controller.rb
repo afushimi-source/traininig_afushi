93c0c9b636f5632900888e5a146f8021324ae55d
afushimi-source <tanafushi@gmail.com>
2021-08-16 13:46:05 +0900

    fix controller and add web page images

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i(show)
   def index
     @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
-
-  private
-    def set_task
-      @task = Task.find(params[:id])
-    end
 end
