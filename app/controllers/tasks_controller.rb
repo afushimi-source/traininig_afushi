1746324ce601045b7ebca1b39bf7d00da0046fb4
afushimi-source <tanafushi@gmail.com>
2021-08-16 17:51:16 +0900

    fix code according to rubocop

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,38 +5,37 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @task = Task.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @task = Task.new(task_params)
     if @task.save
       redirect_to tasks_url
-      flash[:success] = "Task was successfully created!"
+      flash[:success] = 'Task was successfully created!'
     else
       render :new
-      flash[:error] = "An unexpected error has occurred."
+      flash[:error] = 'An unexpected error has occurred.'
     end
   end
 
   def update
     if @task.update(task_params)
       redirect_to @task
-      flash[:success] = "Task was successfully updated!"
-    else 
+      flash[:success] = 'Task was successfully updated!'
+    else
       render :edit
-      flash[:error] = "An unexpected error has occurred."
+      flash[:error] = 'An unexpected error has occurred.'
     end
   end
 
   private
+
   def set_task
     @task = Task.find(params[:id])
   end
