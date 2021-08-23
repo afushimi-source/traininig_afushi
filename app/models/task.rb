98429de4eb0938cc96ac4946e34fbcb20d579cbd
afushimi-source <tanafushi@gmail.com>
2021-08-23 18:28:30 +0900

    fix code according to rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length: {maximum: 30}
-  validates :description, length: {maximum: 600}
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 600 }
 end
