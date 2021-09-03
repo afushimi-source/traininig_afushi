8c9889acd8e622f37e4f5ff457810f6fd1c60d4e
afushimi-source <tanafushi@gmail.com>
2021-09-03 15:04:42 +0900

    add rspec

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,12 +4,31 @@ RSpec.describe 'tasks', type: :system do
   before { driven_by :rack_test }
 
   describe 'index' do
-    it 'is valid order' do
+    before do
       create_list(:task, 3)
-      ordered_tasks_title = Task.all.order(created_at: :desc).pluck(:title)
       visit tasks_path
+    end
+
+    it 'is valid default order' do
+      ordered_tasks_title = Task.all.order(created_at: :desc).pluck(:title)
       titles_in_view = all('tbody tr td[1]').map(&:text)
       expect(titles_in_view).to eq ordered_tasks_title
+    end
+
+    context 'when valid deadline' do
+      it 'order by asc' do
+        ordered_tasks_title_by_asc = Task.all.order(deadline: :asc).pluck(:title)
+        find(:xpath, '//th[contains(.,"deadline")]/a[1]').click
+        titles_in_view = all('tbody tr td[1]').map(&:text)
+        expect(titles_in_view).to eq ordered_tasks_title_by_asc
+      end
+
+      it 'order by desc' do
+        ordered_tasks_title_by_desc = Task.all.order(deadline: :desc).pluck(:title)
+        find(:xpath, '//th[contains(.,"deadline")]/a[2]').click
+        titles_in_view = all('tbody tr td[1]').map(&:text)
+        expect(titles_in_view).to eq ordered_tasks_title_by_desc
+      end
     end
   end
 end
