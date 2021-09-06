336538e2feb9a08fdb308b068e3b56a1b2f28551
afushimi-source <tanafushi@gmail.com>
2021-09-06 10:55:22 +0900

    fix sort button

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,7 @@
 module TasksHelper
   def sort_order(column, direction, params)
-    p params
     content = direction == 'asc' ? '⬆︎' : '⬇︎'
+    content = '' if (direction == 'asc' && params[:direction] == 'asc') or (direction == 'desc' && params[:direction] == 'desc')
     link_to content, { column: column, direction: direction }
   end
 end
