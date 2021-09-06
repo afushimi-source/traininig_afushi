0c6a594a415aaffbf5320acdcf5b82838a6357f7
afushimi-source <tanafushi@gmail.com>
2021-09-06 10:29:53 +0900

    add human_atrtribute_name

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,6 @@
 module TasksHelper
-  def sort_order(column, direction)
+  def sort_order(column, direction, params)
+    p params
     content = direction == 'asc' ? '⬆︎' : '⬇︎'
     link_to content, { column: column, direction: direction }
   end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,10 +4,7 @@ RSpec.describe 'tasks', type: :system do
   before { driven_by :rack_test }
 
   describe 'index' do
-    before do
-      create_list(:task, 3)
-      visit tasks_path
-    end
+    before { create_list(:task, 3); visit tasks_path }
 
     it 'is valid default order' do
       ordered_tasks_title = Task.all.order(created_at: :desc).pluck(:title)
@@ -18,14 +15,14 @@ RSpec.describe 'tasks', type: :system do
     context 'when valid deadline' do
       it 'order by asc' do
         ordered_tasks_title_by_asc = Task.all.order(deadline: :asc).pluck(:title)
-        find(:xpath, '//th[contains(.,"deadline")]/a[1]').click
+        find(:xpath, "//th[contains(.,'#{Task.human_attribute_name(:deadline)}')]/a[1]").click
         titles_in_view = all('tbody tr td[1]').map(&:text)
         expect(titles_in_view).to eq ordered_tasks_title_by_asc
       end
 
       it 'order by desc' do
         ordered_tasks_title_by_desc = Task.all.order(deadline: :desc).pluck(:title)
-        find(:xpath, '//th[contains(.,"deadline")]/a[2]').click
+        find(:xpath, "//th[contains(.,'#{Task.human_attribute_name(:deadline)}')]/a[2]").click
         titles_in_view = all('tbody tr td[1]').map(&:text)
         expect(titles_in_view).to eq ordered_tasks_title_by_desc
       end
