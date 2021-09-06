748e53301e6a60fac9909e3f89bc850a0671cc95
afushimi-source <tanafushi@gmail.com>
2021-09-06 18:25:19 +0900

    fix by rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,6 +10,7 @@ class Task < ApplicationRecord
 
   def self.search(term)
     return Task.all unless term
+
     sanitize_term = "%#{sanitize_sql_like(term)}%"
     Task.where('title LIKE ? OR status LIKE ?', sanitize_term, sanitize_term)
   end
