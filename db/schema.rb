78adab63dab4b018e07009fdcb5b38a1d776ef8a
afushimi-source <tanafushi@gmail.com>
2021-09-06 17:18:08 +0900

    delete extra file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_071004) do
+ActiveRecord::Schema.define(version: 2021_09_03_094106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2021_09_06_071004) do
     t.datetime "updated_at", null: false
     t.date "deadline_on"
     t.string "status", default: "未着手", null: false
-    t.integer "priority", default: 0, null: false
     t.index ["title"], name: "index_tasks_on_title"
   end
 
