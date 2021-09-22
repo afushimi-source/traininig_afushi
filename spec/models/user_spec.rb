1868638b47c7c68b9ddc3965f2dab87314cd8389
afushimi-source <tanafushi@gmail.com>
2021-09-22 17:45:53 +0900

    fix model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,76 +7,64 @@ RSpec.describe User, type: :model do
   end
 
   describe 'name' do
+    let(:user) { FactoryBot.build(:user, name: name) }
+
     context 'when valid' do
-      it '50 characters' do
-        user = FactoryBot.build(:user, name: 'a' * 50)
-        expect(user).to be_valid
-      end
+      let(:name) { 'a' * 50 }
+      it('50 characters') { expect(user).to be_valid }
     end
 
     context 'when invalid' do
-      it 'without a name' do
-        user = FactoryBot.build(:user, name: nil)
-        expect(user).to be_invalid
-      end
+      let(:name) { nil }
+      it('without a name') { expect(user).to be_invalid }
 
-      it '51 characters or more' do
-        user = FactoryBot.build(:user, name: 'a' * 51)
+      let(:name) { 'a' * 51 }
+      it('51 characters or more') { expect(user).to be_invalid }
+    end
+  end
+
+  describe 'email' do
+    let(:user) { FactoryBot.build(:user, email: email) }
+
+    context 'when valid' do
+      let(:email) { "#{'a' * 251}@a.a" }
+      it('255 characters') { expect(user).to be_valid }
+    end
+
+    context 'when invalid' do
+      let(:email) { nil }
+      it('without a email') { expect(user).to be_invalid }
+
+      let(:email) { "#{'a' * 252}@a.a" }
+      it('256 characters or more') { expect(user).to be_invalid }
+
+      let(:email) { 'a' * 6 }
+      it('wrong format email') { expect(user).to be_invalid }
+
+      it 'a duplicate email' do
+        FactoryBot.create(:user, email: 'same@aaa.com')
+        user = FactoryBot.build(:user, email: 'same@aaa.com')
         expect(user).to be_invalid
       end
     end
+  end
 
-    describe 'email' do
-      context 'when valid' do
-        it '255 characters' do
-          user = FactoryBot.build(:user, email: "#{'a' * 251}@a.a")
-          expect(user).to be_valid
-        end
-      end
-
-      context 'when invalid' do
-        it 'without a email' do
-          user = FactoryBot.build(:user, email: nil)
-          expect(user).to be_invalid
-        end
-
-        it '256 characters or more' do
-          user = FactoryBot.build(:user, email: "#{'a' * 252}@a.a")
-          expect(user).to be_invalid
-        end
-
-        it 'wrong format email' do
-          user = FactoryBot.build(:user, email: 'a' * 6)
-          expect(user).to be_invalid
-        end
-
-        it 'a duplicate email' do
-          FactoryBot.create(:user, email: 'same@aaa.com')
-          user = FactoryBot.build(:user, email: 'same@aaa.com')
-          expect(user).to be_invalid
-        end
-      end
+  describe 'password' do
+    let(:user) { FactoryBot.build(:user, password: password, password_confirmation: password_confirmation) }
+    context 'when valid' do
+      let(:password) { 'a' * 6 }
+      let(:password_confirmation) { 'a' * 6 }
+      it('6 characters') { expect(user).to be_valid }
     end
 
-    describe 'password' do
-      context 'when valid' do
-        it '6 characters' do
-          user = FactoryBot.build(:user, password: 'a' * 6, password_confirmation: 'a' * 6)
-          expect(user).to be_valid
-        end
-      end
+    context 'when invalid' do
+      let(:password) { nil }
+      let(:password_confirmation) { nil }
+      it('without a password') { expect(user).to be_invalid }
 
-      context 'when invalid' do
-        it 'without a password' do
-          user = FactoryBot.build(:user, password: nil, password_confirmation: nil)
-          expect(user).to be_invalid
-        end
-
-        it '5 characters or less' do
-          user = FactoryBot.build(:user, password: 'a' * 5, password_confirmation: 'a' * 5)
-          expect(user).to be_invalid
-        end
-      end
+      let(:password) { 'a' * 5 }
+      let(:password_confirmation) { 'a' * 5 }
+      it('5 characters or less') { expect(user).to be_invalid }
     end
   end
 end
