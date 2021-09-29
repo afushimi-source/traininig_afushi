8f1f7bcc6cd1d53a2ccf21d1ef732bf1a9b4947e
afushimi-source <tanafushi@gmail.com>
2021-09-29 17:39:31 +0900

    fix ui/ux

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,20 +1,2 @@
 module UsersHelper
-  def user_type(user)
-    user.is_admin? ? '管理者' : '一般'
-  end
-
-  def toggle_admin(user)
-    if user.is_admin?
-      diplay_text = '一般ユーザーへ'
-      admin_attribute = false
-    else
-      diplay_text = '管理者ユーザへ'
-      admin_attribute = true
-    end
-    link_to diplay_text, admin_user_path(user.id, params: { user: { is_admin: admin_attribute } }), method: :put
-  end
-
-  def display_admin(user)
-    user.is_admin? ? '管理者' : '一般'
-  end
 end
