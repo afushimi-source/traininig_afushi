0c0f06ae3afef5cf430693143aad65e93010b612
afushimi-source <tanafushi@gmail.com>
2021-10-01 14:21:28 +0900

    fix admin controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -31,8 +31,8 @@ class Admin::UsersController < ApplicationController
       flash[:success] = t 'users.flash.destroy_success'
     else
       flash[:danger] = @user.errors[:admin_none].first || t('users.flash.destroy_error')
-      redirect_to admin_users_path
     end
+    redirect_to admin_users_path
   end
 
   private
