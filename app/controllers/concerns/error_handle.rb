1624d10ab3ae98c025b9de680eb9ec897f05d868
afushimi-source <tanafushi@gmail.com>
2021-10-06 10:41:35 +0900

    fix config and error_handler

--- app/controllers/concerns/error_handle.rb
+++ app/controllers/concerns/error_handle.rb
@@ -10,24 +10,24 @@ module ErrorHandle
     rescue_from Unauthorized, with: :rescue401
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
-    def rescue403(exception)
+    def rescue500(exception = nil)
+      logger.error "Rendering 500 with exception: #{exception.message}" if exception
+      render 'errors/500', status: :internal_server_error
+    end
+
+    def rescue403(exception = nil)
       logger.error "Rendering 403 with exception: #{exception.message}" if exception
       render 'errors/forbidden', status: :forbidden
     end
 
-    def rescue401(exception)
+    def rescue401(exception = nil)
       logger.error "Rendering 401 with exception: #{exception.message}" if exception
       render 'errors/unauthorized', status: :unauthorized
     end
 
-    def not_found(exception: nil)
+    def not_found(exception = nil)
       logger.error "Rendering 404 with exception: #{exception.message}" if exception
       render 'errors/404', status: :not_found
-    end
-
-    def rescue500(exception)
-      logger.error "Rendering 500 with exception: #{exception.message}" if exception
-      render 'errors/500', status: :internal_server_error
     end
   end
 end
